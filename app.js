4cbfa98d3d396ac943980d81a1875e686619401e
unknown <dinhhoannguyen999@gmail.com>
2019-07-22 10:52:48 +0700

    update last

---
+++ app.js
@@ -0,0 +1,66 @@
+const ip = require("ip");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+require('./config/passport')(passport); // Passport Config
+
+const answer = require("./models/answer");
+const Question = require("./models/question");
+const Topic = require("./models/topic");
+
+const app = express();
+const ipAdress = process.env.ip || ip.address();
+const port = process.env.port || 3000;
+
+const indexRoutes = require("./routes/index"),
+    userRoutes = require("./routes/users"),
+    //commentsRoutes = require("./routes/comments"),
+    feedRoutes = require("./routes/newFeed"),
+    answerRoutes = require("./routes/answer"),
+    topicRoutes = require("./routes/topics");
+mongoose.connect("mongodb+srv://hoan:7QF4jpNjkMaWuzaO@cluster0-7nvfn.mongodb.net/test?retryWrites=true&w=majority");
+app.use(cookieParser('secret'));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+// Express session
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req, res, next) => {
+    Topic.find({}, (err, topics) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.locals.topics = topics;
+            res.locals.success_msg = req.flash('success_msg');
+            res.locals.error_msg = req.flash('error_msg');
+            res.locals.error = req.flash('error');
+            next();
+        }
+    });
+});
+
+app.use("/", indexRoutes);
+app.use("/feed", feedRoutes);
+app.use("/topic", topicRoutes);
+app.use("/answer", answerRoutes);
+// app.use("/search/",searchRoutes);
+app.use('/', userRoutes);
+app.listen(port, ipAdress, () => {
+    console.log("Server is listening at " + ipAdress + ":" + port);
+});
