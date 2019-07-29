f9b678918495a40c6e9a507f9d558fdab6cf23e2
Hoang <hoang.do@netwpower.no>
2019-07-29 11:19:03 +0700

    follow topic

--- routes/topics.js
+++ routes/topics.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Topic = require("../models/topic");
 const { ensureAuthenticated } = require("../config/auth");
-router.get("/:id",ensureAuthenticated, (req, res) => {
+
+router.get("/:id", ensureAuthenticated, (req, res) => {
   Topic.findById(req.params.id, (err, topic) => {
     if (err) {
       console.log(err);
@@ -12,5 +13,33 @@ router.get("/:id",ensureAuthenticated, (req, res) => {
   });
 });
 
-
+router.get("/:topicId/:userId/:action", (req, res) => {
+  var topicId = req.params.topicId;
+  var userId = req.params.userId;
+  var action = req.params.action;
+  var count;
+  Topic.findById(topicId, (err, topic) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (action === "Follow") {
+        topic.followers.push(userId);
+        topic.save();
+        count = topic.followers.length.toString();
+        action = "Unfollow";
+      } else {
+        for (var i = 0; i < topic.followers.length; i++) {
+          if (topic.followers[i] == userId) {
+            topic.followers.splice(i, 1);
+          }
+        }
+        topic.save();
+        count = topic.followers.length.toString();
+        action = "Follow";
+      }
+      var follow = { count: count, status: action };
+      return res.send(follow);
+    }
+  });
+});
 module.exports = router;
