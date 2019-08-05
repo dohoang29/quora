918256052babe65949097b143217da929329d134
Hoang <hoang.do@netwpower.no>
2019-08-05 15:29:13 +0700

    updating

---
+++ config/key.js
@@ -0,0 +1,19 @@
+module.exports = {
+  google: {
+    clientID:
+      "924040463658-12d0372388vf8j6nk4v3ag6bem0ad9ev.apps.googleusercontent.com",
+    clientSecret: "0N1ID3X4bG4SOqm1l7Ft6ZOc"
+  },
+  session: {
+    cookieKey: "q&aapp"
+  },
+  email: {
+    MAIL_USER: "dinhhoannguyen999@gmail.com",
+    MAIL_PASS: "HHoan1998"
+  },
+  cloudName: {
+    cloud_name: "assignment029",
+    api_key: "229664144827119",
+    api_secret: "lutwdSC3mQpLoqofxO8omDajcXM"
+  }
+};
